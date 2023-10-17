823d64fd5015d2a765ab3f3139963817af6b93be
maria clara bensberg <105284703+mariabrendabensberggggggggggg@users.noreply.github.com>
2023-10-17 09:12:21 -0300

    ok

---
+++ main.js
@@ -0,0 +1,18 @@
+function tocaSomPom ( ){
+
+    document querySelector ( #som tecla pom') .play ();
+    )
+    
+    document. querySelector(' tecla pom') onclick = tocaSomPom;
+    
+    function tocaSomClap () (
+    
+    }
+    
+    document querySelector ('#som tecla clap') play ();
+    
+    document.querySelector('.tecla_clap').onclick=tocaSomClap;
+    
+    const listaDeTeclas = document. querySelectorAll (' tecla');
+    
+    listaDeTeclas (1).onclick = TocaSomClap;
